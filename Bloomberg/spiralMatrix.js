f3bc5a20cf953b9b0dd2f7d6a6efe7b1bbeac31a
Kristina Stefanelli <kristinastefanelli@gmail.com>
2022-03-21 12:29:23 -0400

    bloomberg lc

---
+++ Bloomberg/spiralMatrix.js
@@ -0,0 +1,15 @@
+var spiralOrder = function(matrix) {
+    const res = []
+    while(matrix.length){
+      const first = matrix.shift()
+      res.push(...first)
+      for(const m of matrix){
+        let val = m.pop()
+        if(val)
+          res.push(val)
+          m.reverse()   
+      }
+      matrix.reverse()
+    }
+    return res
+  };
