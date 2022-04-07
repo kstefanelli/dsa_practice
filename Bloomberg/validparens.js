ad071620e372efb59e1ccf0a69da9f3f81209237
Kristina Stefanelli <kristinastefanelli@gmail.com>
2022-04-07 09:32:14 -0400

    valid parens

---
+++ Bloomberg/validparens.js
@@ -0,0 +1,21 @@
+var isValid = function(s) {
+    const openingBrackets = '([{';
+    const closingBrackets = ')]}';
+    const matchingBrackets = {')': '(',']' : '[','}' : '{'};
+    const stack = [];
+    for(const char of s) {
+        if(openingBrackets.includes(char)){
+            stack.push(char);
+        }else if(closingBrackets.includes(char)){
+            if(stack.length === 0){
+                return false;
+            }
+            if(stack[stack.length - 1] === matchingBrackets[char]){
+                stack.pop()
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
